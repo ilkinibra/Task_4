ae763f1635ce99b8427ca0a8278b4f0ee9171ab3
Ilkin Ibrahimov <99792912+ilkinibra@users.noreply.github.com>
2022-04-16 11:30:56 +0400

    Add files via upload

---
+++ Task_4/Task_4/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"MULTIPLY:{Multiply(5, 4, 2)}");
+            Console.WriteLine($"DIVIDE: {Divide(30, 4, 5)}");
+            Console.WriteLine($"MINUS:{Minus(20, 4, 5)}");
+            Console.WriteLine($"SUM:{Sum(2,3,4)}");
+            Console.WriteLine($"DIVIDE: {Divide(30, 4)}");
+        }
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public static int Sum(int a, int b, int c, int d)
+        {
+            return a + b + c + d;
+        }
+        public static int Sum(int a, int b, int c, int d, int e)
+        {
+            return a + b + c + d + e;
+        }
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Minus(int a, int b, int c)
+        {
+            return a - b - c;
+        }
+        public static int Minus(int a, int b, int c, int d)
+        {
+            return a - b - c - d;
+        }
+        public static int Minus(int a, int b, int c, int d, int e)
+        {
+            return a - b - c - d - e;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Multiply(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+        public static int Multiply(int a, int b, int c, int d)
+        {
+            return a * b * c * d;
+        }
+        public static int Multiply(int a, int b, int c, int d, int e)
+        {
+            return a * b * c * d * e;
+        }
+        public static double Divide(int a, int b)
+        {
+            return (double)a / b;
+        }
+        public static double Divide(double a, double b, double c)
+        {
+            return a / b / c;
+        }
+        public static double Divide(double a, double b, double c, double d)
+        {
+            return a / b / c / d;
+        }
+
+        public static double Divide(double a, double b, double c, double d, double e)
+        {
+            return a / b / c / d / e;
+        }
+    }
+}
